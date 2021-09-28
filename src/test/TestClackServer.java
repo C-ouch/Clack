19c050a33cafae802e80e22682f6e69da6b62c55
Elek Ye <tiye@Clarkson.edu>
2021-09-27 20:51:21 -0400

    add param and return Comment

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -3,7 +3,7 @@ import data.ClackData;
 
 /**
  * @author Elek Ye and Evan Couchman
- * A class that represents the client user
+ * A class that represents the client user 
  */
 public class TestClackServer{
 
