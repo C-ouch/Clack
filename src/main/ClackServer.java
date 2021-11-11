c26d0bcaf2441db7c1bedfdf3042ef705bba3b21
C-ouch <couchmec@clarkson.edu>
2021-11-11 18:20:53 -0500

    need part 3.5 & 4.2

--- src/main/ClackServer.java
+++ src/main/ClackServer.java
@@ -1,70 +1,147 @@
 package main;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import data.ClackData;
 import data.MessageClackData;
-public class ClackServer{
+
+public class ClackServer {
 
     /**
-     * @author Elek Ye and Evan Couchman
-     * A class that represents the client user
+     * integer representing port number on server
      */
-
-    /** integer representing port number on server*/
     private int port;
-    /** boolean representing the server is accepting any clients or not*/
+    /**
+     * boolean representing the server is accepting any clients or not
+     */
     private boolean closeConnection;
-    /**private data receive or send to client*/
+    /**
+     * private data receive or send to client
+     */
     private ClackData dataToReceiveFromClient;
     private ClackData dataToSendToClient;
-    /** public constructor set the integer of port witch is 7000*/
+
+    /**
+     * private in and out stream
+     */
+    private ObjectOutputStream outToClient;
+    private ObjectInputStream inFromClient;
+
+
+    /**
+     * public constructor set the integer of port witch is 7000
+     */
     public ClackServer(int port) {
-        if (dataToReceiveFromClient == null|| dataToSendToClient == null ||port <7000)
-        {
+        if (dataToReceiveFromClient == null || dataToSendToClient == null || port < 7000) {
             System.out.println(" port need to above 7000");
-        }else{
-          this.port =port;
-          closeConnection=false;
-          dataToReceiveFromClient=null;
-          dataToSendToClient=null;
+        } else {
+            closeConnection = false;
+            this.port = port;
+            dataToReceiveFromClient = null;
+            dataToSendToClient = null;
+            outToClient = null;
+            inFromClient = null;
         }
 
     }
-    /** public constructor set the integer of port witch is 7000*/
-    public ClackServer(){
+
+    /**
+     * public constructor set the integer of port witch is 7000
+     */
+    public ClackServer() {
         this(7000);
     }
-    /** public constructor set start*/
-    public void start(){
+
+    /**
+     * public constructor set start
+     */
+    public void start() {
+        try {
+            //int clientPort = getPort();
+            ServerSocket sskt = new ServerSocket(port);
+            Socket clientSocket = sskt.accept();
+            outToClient = new ObjectOutputStream(clientSocket.getOutputStream());
+            inFromClient = new ObjectInputStream(clientSocket.getInputStream());
+
+            while (!closeConnection) {
+                receiveData();
+                dataToSendToClient = dataToReceiveFromClient;
+                if (!closeConnection) {
+                    sendData();
+                }
+            }
+
+            inFromClient.close();
+            outToClient.close();
+            sskt.close();
+
+        } catch (IOException ioe) {
+
+            System.err.println(ioe.getMessage());
+        }
 
     }
-   /** public constructor receivedata  from client*/
-    public void receiveData(){
 
-    }
-    /** public constructor senddata  to client*/
-    public void sendData(){
+    /**
+     *need to still see if I need to see what problems needed to be caught
+     */
+    public void receiveData() {
+        try {
+            dataToReceiveFromClient = (ClackData) inFromClient.readObject();
 
+            if (dataToReceiveFromClient.getType() == ClackData.CONSTANT_LOGOUT) {
+                closeConnection = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
-    /** public constructor gets data  from client*/
-    public int getPort(){
+
+    /**
+     *  send data  to client
+     */
+    public void sendData() {
+        try {
+
+            outToClient.writeObject(dataToSendToClient);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     *  gets data  from client
+     */
+    public int getPort() {
         return port;
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         int r = 17;
 
-        r = 37* r + port;
-        r = 37* r + (closeConnection ? 0 : 1);
-        r = 37* r + (dataToSendToClient == null ? 0 : dataToSendToClient.hashCode()); //ClackData Object
-        r = 37* r + (dataToReceiveFromClient == null ? 0 : dataToReceiveFromClient.hashCode()); //ClackData Object
+        r = 37 * r + port;
+        r = 37 * r + (closeConnection ? 0 : 1);
+        r = 37 * r + (dataToSendToClient == null ? 0 : dataToSendToClient.hashCode()); //ClackData Object
+        r = 37 * r + (dataToReceiveFromClient == null ? 0 : dataToReceiveFromClient.hashCode()); //ClackData Object
 
         return r;
     }
+
     @Override
-    public boolean equals(Object other){
+    public boolean equals(Object other) {
         return true;
     }
+
     @Override
-    public String toString(){
+    public String toString() {
         return toString();
     }
 }
